ab913f5d6575c4487258d298a8e229ca0696f8ad
Dev Dennis <Denniskibiworth@gmail.com>
2021-07-28 10:15:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+import AliexScrape from 'aliexscrape';
+var AliexScrape = require('aliexscrape');
 
+AliexScrape('32853590425') // 32853590425 is a productId
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
